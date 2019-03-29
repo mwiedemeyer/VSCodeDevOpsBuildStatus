2bdb548267bbdba3cdbbf0dcdde37ff61392baa9
Marco Wiedemeyer <marco@wiedemeyer.eu>
2019-03-29 14:48:26 +0100

    polish

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# og-devops-status
+## DevOps Build Status Extension
 
-This extension shows the current build agent status of our DevOps Org.
+This extension shows the current build agent status of your DevOps Org in the status bar.
+
+![DevOps Status](/media/devops-status.gif)
+
+It updates on click and automatically in the background when you switch an active editor window.
+
+Icon made by [Darius Dan](https://www.flaticon.com/authors/darius-dan) from [www.flaticon.com](http://www.flaticon.com)
--- src/extension.ts
+++ src/extension.ts
@@ -2,40 +2,108 @@ import * as vscode from 'vscode';
 import request = require("request");
 
 let statusBarItem: vscode.StatusBarItem;
+let devOpsOrg: string;
+let devOpsToken: string;
 
 export function activate(context: vscode.ExtensionContext) {
 
 	const subscriptions = context.subscriptions;
+	const commandId = "showWhoIsRunningABuild";
+	const resetCommandId = "resetDevOpsConfig";
 
-	const commandId = "og.showWhoIsRunningABuild";
+	devOpsOrg = context.globalState.get("DevOpsOrg", "");
+	devOpsToken = context.globalState.get("DevOpsToken", "");
+
+	let isConfigured = (devOpsOrg.length > 0 && devOpsToken.length > 0);
+
+	subscriptions.push(vscode.commands.registerCommand(resetCommandId, () => {
+		context.globalState.update("DevOpsOrg", "");
+		context.globalState.update("DevOpsToken", "");
+		devOpsOrg = "";
+		devOpsToken = "";
+		isConfigured = false;
+		statusBarItem.text = "$(gear) Click here to configure your DevOps Org";
+		vscode.window.showInformationMessage("Token and Org info removed.");
+	}));
+
 	subscriptions.push(vscode.commands.registerCommand(commandId, () => {
-		updateStatusBarItem();
+		if (isConfigured) {
+			updateStatusBarItem();
+		} else {
+			vscode.window.showInputBox({
+				prompt: "Please enter your DevOps Organization (https://dev.azure.com/ORG)",
+				placeHolder: "ORG",
+			}).then((org) => {
+				if (org) {
+					devOpsOrg = org;
+					vscode.window.showInputBox({
+						prompt: "Please enter your 'Full Access' personal access token",
+						placeHolder: "TOKEN from DevOps->Security->Personal Access Tokens"
+					}).then((token) => {
+						if (token) {
+							devOpsToken = token;
+
+							context.globalState.update("DevOpsOrg", devOpsOrg).then(() => {
+								context.globalState.update("DevOpsToken", devOpsToken).then(() => {
+									updateStatusBarItem();
+								});
+							});
+							isConfigured = true;
+							statusBarItem.text = "$(sync) Loading...";
+						}
+					});
+				}
+			});
+		}
 	}));
 
 	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
 	statusBarItem.command = commandId;
-	statusBarItem.text = "$(sync) Who is running a build?";
+
+	if (isConfigured) {
+		statusBarItem.text = "$(sync) Loading...";
+	} else {
+		statusBarItem.text = "$(gear) Click here to configure your DevOps Org";
+	}
+
 	subscriptions.push(statusBarItem);
 	statusBarItem.show();
 
 	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+
+	updateStatusBarItem();
 }
 
 function updateStatusBarItem() {
-	loadDevOpsBuildInfo().then((s) => {
-		statusBarItem.text = s;
-	});
-}
+	request(`https://dev.azure.com/${devOpsOrg}/_apis/distributedtask/resourceusage?parallelismTag=Private&poolIsHosted=true&includeRunningRequests=true`, {
+		auth: {
+			username: "dummy",
+			password: devOpsToken
+		}
+	}, (error, response, body) => {
+		const data = JSON.parse(body);
 
-async function loadDevOpsBuildInfo(): Promise<string> {
-	return new Promise<string>(resolve => {
-		request("https://ogccc-devops-tools.azurewebsites.net/api/DevopsBuilds?code=nWnmT3fnYFnZnZMOZa1GDkF2eb1kiC0YQ8263/oHmeqnFxXC0FMm8Q==", (error, response, body) => {
-			const data = JSON.parse(body);
-			if (data.isRunning) {
-				resolve(`$(clock) ${data.owner} is building '${data.buildTitle}'`);
+		if (data.usedCount > 0 && data.runningRequests && data.runningRequests.length > 0) {
+
+			if (data.runningRequests.length > 1) {
+				statusBarItem.text = `$(clock) ${data.runningRequests.length} running builds`;
 			} else {
-				resolve("$(thumbsup) No running builds");
+				const buildTitle = data.runningRequests[0].definition.name;
+				const buildDetailsUrl = data.runningRequests[0].owner._links.self.href;
+
+				request(buildDetailsUrl, {
+					auth: {
+						username: "dummy",
+						password: devOpsToken
+					}
+				}, (error2, response2, body2) => {
+					const dataDetails = JSON.parse(body2);
+					const owner = dataDetails.requestedFor.displayName;
+					statusBarItem.text = `$(clock) ${owner} is building '${buildTitle}'`;
+				});
 			}
-		});
+		} else {
+			statusBarItem.text = "$(thumbsup) No running builds";
+		}
 	});
 }
